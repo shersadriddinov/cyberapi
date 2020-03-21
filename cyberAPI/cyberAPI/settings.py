eb27f740dbf8d60c4455ac0933da6f2bd3e1ed1a
shersadriddinov <sher.sadriddinov@gmail.com>
2020-03-21 14:02:23 +0500

    i18n added

--- cyberAPI/cyberAPI/settings.py
+++ cyberAPI/cyberAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -44,16 +45,19 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'django.contrib.admindocs'
 ]
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.contrib.admindocs.middleware.XViewMiddleware',
 ]
 
 ROOT_URLCONF = 'cyberAPI.urls'
@@ -69,6 +73,7 @@ TEMPLATES = [
 				'django.template.context_processors.request',
 				'django.contrib.auth.context_processors.auth',
 				'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.i18n',
 			],
 		},
 	},
@@ -122,6 +127,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = (
+	('en', _('English')),
+	('ru', _('Russian')),
+)
+
+LOCALE_PATHS = (
+	os.path.join(BASE_DIR, 'locale'),
+)
 
 TIME_ZONE = 'Asia/Tashkent'
 
--- cyberAPI/v1/models.py
+++ cyberAPI/v1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+	"""
+	Information about user, works in one to one link with default :model:User
+	"""
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	balance = models.PositiveIntegerField(
+		help_text=_('Coins earned by playing'),
+		verbose_name=_("Game Balance"),
+	)
+	donate = models.PositiveIntegerField(
+		help_text=_('Coins obtained by donations'),
+		verbose_name=_("Donate Balance")
+	)
+
+	# Fields to add
+	# character M2M
+
