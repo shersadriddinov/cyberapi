d10b0570459aee7323c3cce6686740cbc8566e36
shersadriddinov <sher.sadriddinov@gmail.com>
2020-03-21 14:30:32 +0500

    Profile model created

--- cyberAPI/v1/models.py
+++ cyberAPI/v1/models.py
@@ -5,18 +5,30 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 	"""
-	Information about user, works in one to one link with default :model:User
+	Information about user, works in one to one link with default :model:`User`
 	"""
+	# TODO: add field character (M2M)
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	balance = models.PositiveIntegerField(
+		db_column='balance',
+		null=False,
+		default=0,
+		blank=True,
 		help_text=_('Coins earned by playing'),
 		verbose_name=_("Game Balance"),
 	)
 	donate = models.PositiveIntegerField(
+		db_column='column',
+		null=False,
+		default=0,
+		blank=True,
 		help_text=_('Coins obtained by donations'),
 		verbose_name=_("Donate Balance")
 	)
 
-	# Fields to add
-	# character M2M
+	class Meta:
+		db_table = 'user_balance'
+		verbose_name = _('User Balance')
+		verbose_name_plural = _('User Balance')
+
 
