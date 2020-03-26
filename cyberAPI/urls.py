79e29558ac6bc3f9cc542e46b70be0af96d9a391
shersadriddinov <sher.sadriddinov@gmail.com>
2020-03-26 21:07:44 +0500

    disable i18n

--- cyberAPI/urls.py
+++ cyberAPI/urls.py
@@ -5,10 +5,13 @@ from django.urls import include, path
 
 urlpatterns = [
     path('api/', include('v1.urls')),
-    path('api-token-auth/', obtain_auth_token)
-]
-
-urlpatterns += i18n_patterns(
+    path('api-token-auth/', obtain_auth_token),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-)
+]
+
+# Until Great Times!
+# urlpatterns += i18n_patterns(
+#     path('admin/doc/', include('django.contrib.admindocs.urls')),
+#     path('admin/', admin.site.urls),
+# )
