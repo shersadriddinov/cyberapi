32baec296b0bacf1d6ffdf1ad3edbba4caf9b438
shersadriddinov <sher.sadriddinov@gmail.com>
2020-07-28 22:40:57 +0500

    new serializer for notifs

--- socket_handler/serializers.py
+++ socket_handler/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 from socket_handler.models import *
 
 
+class UserNotifSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ("id", "first_name", )
+
+
 class NotificationSerializer(serializers.ModelSerializer):
+	user = UserNotifSerializer()
+	friend_id = UserNotifSerializer()
+
 	class Meta:
 		model = Notification
 		fields = "__all__"
