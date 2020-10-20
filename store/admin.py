959c36b2076d209295a9d0ad30140916401a88c0
shersadriddinov <sher.sadriddinov@gmail.com>
2020-10-20 16:06:22 +0500

    LotAdmin & UserLot admin added

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from store.models import *
 
 
+class LotAdmin(admin.ModelAdmin):
+	list_display = ('id', 'tech_name', 'status', "date_created", "premium", "price")
+	list_display_links = ("tech_name", )
+	list_select_related = True
+	list_editable = ('status', 'premium', "price")
+	list_filter = ('status', 'premium', 'date_created')
+	ordering = ('-date_created',)
+	search_fields = ("tech_name", )
+	filter_horizontal = ('character', 'weapons', 'stock', 'barrel', 'muzzle', 'mag', 'scope', 'grip')
+	fieldsets = (
+		(None, {'fields': ('tech_name', 'status', ('premium', 'price'), 'date_created')}),
+		(_("Collection"), {'fields': ('character', 'weapons', 'stock', 'barrel', 'muzzle', 'mag', 'scope', 'grip')})
+	)
+
+
+class UserLotAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'lot', 'date_purchased')
+	list_display_links = ('id', )
+	list_filter = ('lot', )
+	search_fields = ('user', 'lot')
+	readonly_fields = ('date_purchased', 'user', 'lot')
+	ordering = ('-date_purchased',)
+
+
+admin.site.register(Lot, LotAdmin)
+admin.site.register(UserLots, UserLotAdmin)
